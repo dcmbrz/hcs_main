dcf20b939323528ea21dfc7310222d80bee89204
dcmbrz <ekwuemem@uci.edu>
2025-11-16 19:35:49 -0800

    working on admin dashboard
    currently getting 404 error on the localhost page

---
+++ lib/db.ts
@@ -0,0 +1,21 @@
+// Import Prisma Client
+import { PrismaClient } from "@prisma/client";
+
+// Create singleton function
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+// TypeScript type definition
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+// The actual client instance
+const db = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+// Export for use in your app
+export default db;
+
+// Store in globalThis only in development
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = db;
