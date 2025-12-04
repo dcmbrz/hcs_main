d56a13a4d5b84dde37aab1770dcf4b4172664aea
dcmbrz <ekwuemem@uci.edu>
2025-12-03 22:16:23 -0800

    worked on redirecting new patients to the registration page

---
+++ app/(protected)/patient/registration/pages.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const PatientDashboard = async() => {
+
+    const user = await currentUser();
+
+    const data = null;
+
+    if (user && !data) {
+        redirect("/patient/registration")
+    }
+
+    return (
+        <div>
+            PatientDashboard
+            <UserButton  />
+        </div>
+    );
+};
+
+export default PatientDashboard;
